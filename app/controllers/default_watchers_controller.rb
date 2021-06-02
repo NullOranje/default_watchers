9e6c8d8135d9ccf9499fbf013f8f203f2a3ae5ca
Nick McKinney <nmckinney@breakpoint-labs.com>
2021-06-02 13:26:45 -0400

    Remove unused classes

--- app/controllers/default_watchers_controller.rb
+++
@@ -1,8 +0,0 @@
-class DefaultWatchersController < ApplicationController
-  unloadable
-
-  def index
-    @all_active_roles = Role.givable.find(:all)
-  end
-
-end
--- app/models/default_watcher.rb
+++
@@ -1,4 +0,0 @@
-class DefaultWatchers < ActiveRecord::Base
-  belongs_to :role
-  belongs_to :watcher, :class_name => "Role"
-end

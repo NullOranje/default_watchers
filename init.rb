698fa52e89ed47fc04b19a274407635776be3a02
Nick McKinney <nmckinney@breakpoint-labs.com>
2021-06-03 10:13:55 -0400

    Use https links for author URL

--- init.rb
+++ init.rb
@@ -5,7 +5,7 @@ Redmine::Plugin.register :default_watchers do
   author 'Nick McKinney'
   description 'Automatically add users as watchers to tickets based on role(s).'
   version '0.1.1'
-  url 'http://github.com/NullOranje/default_watchers'
-  author_url 'http://github.com/NullOranje/'
+  url 'https://github.com/NullOranje/default_watchers'
+  author_url 'https://github.com/NullOranje/'
   settings default: {'default_watchers' => []}, partial: 'settings/default_watchers'
 end

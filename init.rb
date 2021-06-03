b6edd97ef33219bd20321c1eff217176de5a6895
Nick McKinney <nmckinney@breakpoint-labs.com>
2021-06-03 10:08:24 -0400

    Bump to version v0.1.1

--- init.rb
+++ init.rb
@@ -4,7 +4,7 @@ Redmine::Plugin.register :default_watchers do
   name 'Default Watchers plugin'
   author 'Nick McKinney'
   description 'Automatically add users as watchers to tickets based on role(s).'
-  version '0.1.0'
+  version '0.1.1'
   url 'http://github.com/NullOranje/default_watchers'
   author_url 'http://github.com/NullOranje/'
   settings default: {'default_watchers' => []}, partial: 'settings/default_watchers'
